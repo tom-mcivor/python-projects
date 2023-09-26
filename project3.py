6cfa7242242811dde48d7c74bfbc496302d0be7d
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-09-27 11:42:17 +1300

    add

--- project3.py
+++ project3.py
@@ -1,16 +1,38 @@
 import random
+import time
 
-OPERATORS = ["+", "-", "/", "*"]
-MIN_OPERAND = 3 
-MAX_OPERAND = 12 
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
 
 def generate_problem():
-  left = random.randint(MIN_OPERAND, MAX_OPERAND)
-  right = random.randint(MIN_OPERAND, MAX_OPERAND)
-  operator = random.choice(OPERATORS)
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
 
-  expr = str(left) + " " + right + " " + str(right)
-  print()
-  return expr
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem
+
+wrong = 0
+input("Press enter to start!")
+print("----------------------")
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("----------------------")
+print("Nice work! You finished in", total_time, "seconds!")
